4d34c92ada9f365fca5957a663eb5875bcdc12e9
jackelinepao <jackelipao@gmail.com>
2019-09-23 22:09:09 -0500

    add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductoComponent } from './components/producto/producto.component'
+import { HeaderComponent } from './components/header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductoComponent
+    ProductoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
